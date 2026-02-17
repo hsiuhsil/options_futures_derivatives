09d6aaf907715067b1e6d43dd0e64d1246a4428f
hsiuhsil <hsiuhsil@alumni.cmu.edu>
2026-02-17 15:02:32 -0800

    Implement Black-Scholes-Merton model and update notebook imports

---
+++ src/black_scholes_merton.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.stats import norm
+
+def cumulative_prob(x):
+    return norm.cdf(x, loc=0, scale=1)
+
+def BSM_pricing(option_type, exercise_style, s, k, T, r, q, sigma):
+
+    if exercise_style!='European':
+        raise ValueError("BSM does not apply to American put options")
+    if sigma <= 0 or T <= 0:
+        raise ValueError("Volatility and maturity must be positive.")
+
+    d1 = (np.log(s/k)+(r-q+0.5*sigma**2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+
+    if option_type=='call':
+        return s*np.exp(-q*T)*cumulative_prob(d1) - k*np.exp(-r*T)*cumulative_prob(d2)
+    elif option_type=='put':
+        return -s*np.exp(-q*T)*cumulative_prob(-d1) + k*np.exp(-r*T)*cumulative_prob(-d2)
+    else:
+        raise ValueError("option_type must be 'call' or 'put'")
