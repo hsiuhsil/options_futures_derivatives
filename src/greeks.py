b4a36ffc31763871be91ccb8e42d4488301c8241
hsiuhsil <hsiuhsil@alumni.cmu.edu>
2026-02-18 15:25:03 -0800

    Implement Black-Scholes Theta in greeks module

--- src/greeks.py
+++ src/greeks.py
@@ -1,12 +1,27 @@
+import numpy as np
+from scipy.stats import norm
 from black_scholes_merton import cumulative_prob, calculate_d1_d2
 
 def delta(option_type, s, k, r, sigma, T):
 
+    if T <= 0:
+        raise ValueError("Maturity must be positive.")
     d1, d2 = calculate_d1_d2(s, k, r, sigma, T, q=0)
 
     if option_type=='call':
         return cumulative_prob(d1)
     elif option_type=='put':
-        return cumulative_prob(-d1) - 1
+        return cumulative_prob(d1) - 1
+
+def theta(option_type, s, k, r, sigma, T):
+
+    if T <= 0:
+        raise ValueError("Maturity must be positive.")
+    d1, d2 = calculate_d1_d2(s, k, r, sigma, T, q=0)
+    
+    if option_type=='call':
+        return -s*norm.pdf(d1)*sigma/(2*np.sqrt(T)) - r*k*np.exp(-r*T)*cumulative_prob(d2)
+    elif option_type=='put':
+        return -s*norm.pdf(d1)*sigma/(2*np.sqrt(T)) + r*k*np.exp(-r*T)*cumulative_prob(-d2)
 
 
