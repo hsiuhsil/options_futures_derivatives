864274b6713faaa31d9d4d41f59ca01e10c83be5
hsiuhsil <hsiuhsil@alumni.cmu.edu>
2026-02-19 13:23:08 -0800

    add vega and rho to greeks module

--- src/greeks.py
+++ src/greeks.py
@@ -4,6 +4,8 @@ from black_scholes_merton import cumulative_prob, calculate_d1_d2
 
 def delta(option_type, s, k, r, sigma, T):
 
+    if sigma <= 0:
+        raise ValueError("Volatility must be positive.")
     if T <= 0:
         raise ValueError("Maturity must be positive.")
     d1, d2 = calculate_d1_d2(s, k, r, sigma, T, q=0)
@@ -12,9 +14,13 @@ def delta(option_type, s, k, r, sigma, T):
         return cumulative_prob(d1)
     elif option_type=='put':
         return -cumulative_prob(-d1)
+    else:
+        raise ValueError("option_type must be 'call' or 'put'")
 
 def theta(option_type, s, k, r, sigma, T):
 
+    if sigma <= 0:
+        raise ValueError("Volatility must be positive.")
     if T <= 0:
         raise ValueError("Maturity must be positive.")
     d1, d2 = calculate_d1_d2(s, k, r, sigma, T, q=0)
@@ -23,6 +29,8 @@ def theta(option_type, s, k, r, sigma, T):
         return -s*norm.pdf(d1)*sigma/(2*np.sqrt(T)) - r*k*np.exp(-r*T)*cumulative_prob(d2)
     elif option_type=='put':
         return -s*norm.pdf(d1)*sigma/(2*np.sqrt(T)) + r*k*np.exp(-r*T)*cumulative_prob(-d2)
+    else:
+        raise ValueError("option_type must be 'call' or 'put'")
 
 def gamma(s, k, r, sigma, T):
     """
@@ -38,7 +46,60 @@ def gamma(s, k, r, sigma, T):
     Returns:
         float: Option gamma
     """
+    if sigma <= 0:
+        raise ValueError("Volatility must be positive.")
     if T <= 0:
         raise ValueError("Maturity must be positive.")
     d1, _ = calculate_d1_d2(s, k, r, sigma, T, q=0)
     return norm.pdf(d1) / (s*sigma*np.sqrt(T))
+
+def vega(s, k, r, sigma, T):
+    """
+    Compute the vega of a European option (call or put).
+    
+    Parameters:
+        s (float): Current stock price
+        k (float): Strike price
+        r (float): Risk-free rate
+        sigma (float): Volatility
+        T (float): Time to maturity (years)
+ 
+    Returns:
+        float: Option vega (per 1% change in volatility)
+    """
+
+    if sigma <= 0:
+        raise ValueError("Volatility must be positive.")
+    if T <= 0:
+        raise ValueError("Maturity must be positive.")
+    d1, _ = calculate_d1_d2(s, k, r, sigma, T, q=0)
+    return s*np.sqrt(T)*norm.pdf(d1)/100
+
+def rho(option_type, s, k, r, sigma, T):
+    """
+    Compute the rho of a European call option or a European put option.
+    
+    Parameters:
+        option_type (string): either 'call' or 'put'
+        s (float): Current stock price
+        k (float): Strike price
+        r (float): Risk-free rate
+        sigma (float): Volatility
+        T (float): Time to maturity (years)
+ 
+    Returns:
+        float: Option rho
+    """
+
+    if sigma <= 0:
+        raise ValueError("Volatility must be positive.")
+    if T <= 0:
+        raise ValueError("Maturity must be positive.")
+    _, d2 = calculate_d1_d2(s, k, r, sigma, T, q=0)
+   
+    if option_type=='call':
+        return k*T*np.exp(-r*T)*cumulative_prob(d2)
+    elif option_type=='put':
+        return -k*T*np.exp(-r*T)*cumulative_prob(-d2)
+    else:
+        raise ValueError("option_type must be 'call' or 'put'")
